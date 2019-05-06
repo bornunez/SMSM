1502a5ed660b77d7a25bc663605e2d227318e819
Alberto <casadotrapote98@gmail.com>
2019-05-07 00:19:06 +0200

    Alberto C: Escopeta (sin terminar)
    Le falta que las balas se "esparzan"

--- Src/Game/Armas/Weapon.cpp
+++ Src/Game/Armas/Weapon.cpp
@@ -143,17 +143,17 @@ void Weapon::PhysicShoot()
 	{
 		for (int i = 0; i < bulletsAmount; i++)
 		{
-			int disp = dispersion;
-			float randX = rand() % (disp)-(disp / 2);
-			float randY = rand() % (disp)-(disp / 2);
+			//int disp = dispersion;
+			//float randX = rand() % (disp)-(disp / 2);
+			//float randY = rand() % (disp)-(disp / 2);
 			//scene->Instantiate("Bullet",(gameObject->getPosition() + Vector3(randX*0.01, 0.05 + randY * 0.01, -0.5)), 0.01);
-			directionalShoot();
+			directionalShoot(dispersion);
 		}
 	}
 	else
 	{
 		//scene->Instantiate("Bullet", (gameObject->getPosition() + Vector3(0, 0.05, -0.5)), 0.01);
-		directionalShoot();
+		directionalShoot(0);
 	}
 	//cout << "Disparo en: [ " <<gameObject->getGlobalPosition().x << ", "<< gameObject->getGlobalPosition().y <<", " << gameObject->getGlobalPosition().z << " ]" << endl;
 	
@@ -161,13 +161,16 @@ void Weapon::PhysicShoot()
 
 }
 
-void Weapon::directionalShoot()
+void Weapon::directionalShoot(float randOff)
 {
 	Vector3 auxVec = scene->getGame()->getViewport()->getCamera()->getRealOrientation() * Vector3::NEGATIVE_UNIT_Z*offset.z;
 	//Vector3 auxVec = gameObject->getParent()->getParent()->getNode()->getOrientation()*Vector3::NEGATIVE_UNIT_Z;
 	//Vector3 auxVec2 = gameObject->getParent()->getNode()->getOrientation()*Vector3::NEGATIVE_UNIT_Z;
 	//float angle2 = atan(auxVec2.y / auxVec2.z)* -57.2958;
 	Vector3 auxVecFinal = Quaternion(Degree(offset.x), Vector3::UNIT_Y) * auxVec;
+	Vector3 auxOff = auxVecFinal.perpendicular();
+	auxOff = Quaternion(Degree(rand() % 360), auxVecFinal) * auxOff;
+	auxVecFinal = auxVecFinal + (auxOff* randOff);
 	//auxVecFinal = Quaternion(Degree(offset.x), Vector3::UNIT_Y) * auxVecFinal;
 	//auxVecFinal.normalise();
 	//auxVecFinal *= offset.z;
