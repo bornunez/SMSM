b4a39b2dadb34c4872c5b686d77a287fdd86ae29
Unknown <dimart10@ucm.es>
2019-05-10 19:40:52 +0200

    Diego M: Estructura bombGuy, esperando mesh

---
+++ Src/Game/Componentes/Enemies/BombGuy.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Enemy.h"
+#include "../../Src/MotorEngine/TimeManager.h"
+
+class MeshRenderer;
+class BombGuy :
+	public Enemy
+{
+private:
+	enum state{IDLE, CHASING, EXPLODING, DEAD};
+	state estado = state::IDLE;
+	MeshRenderer* meshRend = nullptr;
+	float chaseDist = 0;
+	float expDist = 0;
+	float expTime = 0;
+	float expRadius = 0;
+	float scale = 0;
+
+	float expTimer = 0;	
+	TimeManager* tm = nullptr;
+
+public:
+	void Start();
+	BombGuy(GameObject* obj) : Enemy(obj) {};
+	~BombGuy();
+	void LoadFromFile(json obj);
+	void Update();
+	void OnDeath();
+	void Spawn();
+	void Explode();
+};
+
