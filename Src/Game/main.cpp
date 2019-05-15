dd3a040fafb9d815d28aad170243a93d58156327
Borja Núñez Béjar <bornunez@ucm.es>
2019-05-16 00:47:16 +0200

    Borja N. No se muestra la consola.
    Ya no se muestra la consola al ejecutar.

--- Src/Game/main.cpp
+++ Src/Game/main.cpp
@@ -15,7 +15,7 @@
 
 
 int main() {
-
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
 	MyComponentLoader* mCompLoader = new MyComponentLoader();
